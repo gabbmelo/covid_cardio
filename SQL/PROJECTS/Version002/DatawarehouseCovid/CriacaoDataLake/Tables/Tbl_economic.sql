84545dac23e11d67e4ce111c39c43ba3bbc642bf
Gabriel_Melo <gabriel.meloesilva@protonmail.com>
2024-01-09 17:23:52 -0300

    updating folder structure

---
+++ SQL/PROJECTS/Version002/DatawarehouseCovid/CriacaoDataLake/Tables/Tbl_economic.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Tbl_economic]
+(
+	[level_gdp_per_capita] INT NOT NULL PRIMARY KEY, 
+    [desc_level_gdp_per_capita] VARCHAR(3) NOT NULL,
+	[gdp_level_desc] VARCHAR(200) NOT NULL,
+    [gdp_per_capita] FLOAT NOT NULL,
+    [atr_population] INT NOT NULL, 
+    [origin] VARCHAR(200) NOT NULL, 
+    [load_date] DATETIME NOT NULL
+)
---
+++ SQL/PROJECTS/Version003/DatawarehouseCovid/CriacaoDataLake/Tables/Tbl_geo.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Tbl_geo]
+(
+	[geo_country_id] INT NOT NULL PRIMARY KEY,
+	[sub_region_code] INT NOT NULL,
+    [continent_code] INT NOT NULL,
+    [country_code] VARCHAR(3) NOT NULL,
+    [country_desc] VARCHAR(200) NOT NULL,
+    [sub_region_desc] VARCHAR(50) NOT NULL,
+    [continent_desc] VARCHAR(50) NOT NULL,
+    [origin] VARCHAR(200) NOT NULL, 
+    [load_date] DATETIME NOT NULL
+)

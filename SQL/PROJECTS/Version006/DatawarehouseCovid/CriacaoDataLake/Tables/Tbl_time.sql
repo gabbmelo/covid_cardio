d5219a0bd9339d0e9e389e9400d597fb36797212
Gabriel_Melo <gabriel.meloesilva@protonmail.com>
2024-02-08 16:18:56 -0300

    Completed data lake loading process

---
+++ SQL/PROJECTS/Version006/DatawarehouseCovid/CriacaoDataLake/Tables/Tbl_time.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table2]
+(
+	[time_date_id] INT NOT NULL PRIMARY KEY,
+	[date_code] VARCHAR(25) NOT NULL,
+    [date_datetime] DATETIME NOT NULL,
+    [origin] VARCHAR(200) NOT NULL, 
+    [load_date] DATETIME NOT NULL
+)
